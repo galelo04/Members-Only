182020cb3bdd308025041606bfeae8d8a8871810
Mostafa Abdelglel <14712022100624@stud.cu.edu.eg>
2025-02-02 23:36:18 +0200

    feat: sign up user in the database

--- db/queries.js
+++ db/queries.js
@@ -1,1 +1,11 @@
 const pool = require('./pool');
+
+const createUser = async (fname, lname, email, password) => {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  await pool.query(
+    'insert into users (fname,lname,email,password) values ($1, $2, $3, $4)',
+    [fname, lname, email, hashedPassword]
+  );
+};
+
+module.exports = { createUser };
