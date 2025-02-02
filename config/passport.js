630ca94e9d6aef4aac527f14a6409848545459c5
Mostafa Abdelglel <14712022100624@stud.cu.edu.eg>
2025-02-02 21:35:49 +0200

    chore: config of db and sessions and passport

---
+++ config/passport.js
@@ -0,0 +1,50 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+    },
+    async (email, password, done) => {
+      try {
+        const { rows } = await pool.query(
+          'SELECT * FROM users WHERE email = $1',
+          [email]
+        );
+        const user = rows[0];
+
+        if (!user) {
+          return done(null, false, { message: 'Incorrect email' });
+        }
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+          // passwords do not match!
+          return done(null, false, { message: 'Incorrect password' });
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query('SELECT * FROM users WHERE id = $1', [
+      id,
+    ]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
---
+++ db/populatedb.js
@@ -0,0 +1,42 @@
+const { Client } = require('pg');
+require('dotenv').config();
+const { USER, HOST, PORT, PASSWORD, DATABASE } = process.env;
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  fname VARCHAR(255) NOT NULL,
+  lname VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR(255) NOT NULL,
+  timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  text TEXT NOT NULL,
+  user_id INTEGER,
+  FOREIGN KEY (user_id) REFERENCES users
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
+);
+
+`;
+
+async function main() {
+  console.log('seeding...');
+  const client = new Client({
+    host: HOST, // or wherever the db is hosted
+    user: USER,
+    database: DATABASE,
+    password: PASSWORD,
+    port: PORT, // The default port
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log('done');
+}
+
+main();
