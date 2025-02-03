15899695424d12fbe3b69d69acfa7fcf6b17bae5
Mostafa Abdelglel <14712022100624@stud.cu.edu.eg>
2025-02-03 02:12:58 +0200

    feat: logout logic done

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -17,33 +17,12 @@ indexRouter.post(
   })
 );
 
-// router.get('/sign-up', (req, res) => res.render('sign-up-form'));
-
-// router.post('/sign-up', async (req, res, next) => {
-//   try {
-//     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-//     await pool.query('insert into users (username, password) values ($1, $2)', [
-//       req.body.username,
-//       hashedPassword,
-//     ]);
-//     res.redirect('/');
-//   } catch (err) {
-//     return next(err);
-//   }
-// });
-// router.post(
-//   '/log-in',
-//   passport.authenticate('local', {
-//     successRedirect: '/',
-//     failureRedirect: '/',
-//   })
-// );
-// router.get('/log-out', (req, res, next) => {
-//   req.logout((err) => {
-//     if (err) {
-//       return next(err);
-//     }
-//     res.redirect('/');
-//   });
-// });
+indexRouter.get('/log-out', (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+});
 module.exports = indexRouter;
