ad45e04844a6eb176eb3be043c57ec0c6374e1f9
Mostafa Abdelglel <14712022100624@stud.cu.edu.eg>
2025-02-03 20:04:24 +0200

    config: added deployed database instead of local

--- db/pool.js
+++ db/pool.js
@@ -1,13 +1,26 @@
 const { Pool } = require('pg');
 require('dotenv').config();
-const { USER, HOST, PORT, PASSWORD, DATABASE } = process.env;
+const {
+  USER,
+  HOST,
+  PORT,
+  PASSWORD,
+  DATABASE,
+  PGHOST,
+  PGDATABASE,
+  PGUSER,
+  PGPASSWORD,
+} = process.env;
 
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
 module.exports = new Pool({
-  host: HOST, // or wherever the db is hosted
-  user: USER,
-  database: DATABASE,
-  password: PASSWORD,
+  host: PGHOST, // or wherever the db is hosted
+  user: PGUSER,
+  database: PGDATABASE,
+  password: PGPASSWORD,
   port: PORT, // The default port
+  ssl: {
+    require: true,
+  },
 });
