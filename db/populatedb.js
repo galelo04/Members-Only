87ce85711fd874425ef1a9760762d862fc985f01
Mostafa Abdelglel <14712022100624@stud.cu.edu.eg>
2025-02-03 04:09:53 +0200

    feat: new message view

--- db/populatedb.js
+++ db/populatedb.js
@@ -8,7 +8,9 @@ CREATE TABLE IF NOT EXISTS users (
   fname VARCHAR(255) NOT NULL,
   lname VARCHAR(255) NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL
+  password VARCHAR(255) NOT NULL,
+  status varchar(50) NOT NULL DEFAULT 'not authorized',
+  isAdmin BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE IF NOT EXISTS messages (
